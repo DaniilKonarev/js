78086185ed553b99bac5ae54ddfaba08a31fff35
DaniilKonarev <danek32@mail.ru>
2021-09-02 10:04:11 +0300

    added whiles

--- js/script.js
+++ js/script.js
@@ -7,13 +7,30 @@ let numberOfFilms = prompt("Сколько фильмов вы уже посмо
 		privat: false,
 	};
 
-const a = prompt("Какой фильм вы смотрели последним?"),
-		b = prompt("на сколько оцените его?"),
-		c = prompt("Какой фильм вы смотрели последним?"),
-		d = prompt("на сколько оцените его?");		
 
-pesonalMovieDB.movies[a] = b;
-pesonalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+	const a = prompt("Какой фильм вы смотрели последним?", ""),
+		b = prompt("на сколько оцените его?", "");
+
+	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+		pesonalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+
+}
+
+if (pesonalMovieDB.count < 10) {
+	alert("Маловато фильмов");
+} else if (pesonalMovieDB.count >= 10 && pesonalMovieDB.count < 30) {
+	alert("Вы - киноман");
+} else if (pesonalMovieDB.count >= 30) {
+	alert("Ебать вы задрот!!!");
+} else {
+	alert('error');
+}
 
 console.log(pesonalMovieDB);
 
